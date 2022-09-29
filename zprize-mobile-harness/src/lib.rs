aa07e23ea895ab4489b1b7e2054e08ef2994f0d2
Nick Stathas <nickstathas@outlook.com>
2022-09-29 00:29:02 -0700

    Use local MSM impl because traits are hard

--- zprize-mobile-harness/src/lib.rs
+++ zprize-mobile-harness/src/lib.rs
@@ -1,12 +1,14 @@
 use ark_bls12_377 as bls377;
+use ark_ec::Group;
 use ark_ff::fields::Field;
+use ark_ff::BigInteger;
 use ark_ff::PrimeField;
 use ark_serialize::CanonicalDeserialize;
 use ark_serialize::CanonicalSerialize;
 use ark_serialize::Compress;
 use ark_std::io::Write;
 use ark_std::rand::Rng;
-use ark_std::Zero;
+use ark_std::{One, Zero};
 use rand::RngCore;
 use std::collections::VecDeque;
 use std::fs::File;
@@ -27,8 +29,109 @@ pub enum HarnessError {
 }
 
 type Point = bls377::G1Affine;
-type Scalar = <bls377::Fr as PrimeField>::BigInt;
+type ProjectivePoint = bls377::G1Projective;
+type ScalarField = bls377::Fr;
+type Scalar = <ScalarField as PrimeField>::BigInt;
 type Instance = (Vec<Point>, Vec<Scalar>);
+
+fn ln_without_floats(a: usize) -> usize {
+    // log2(a) * ln(2)
+    (ark_std::log2(a) * 69 / 100) as usize
+}
+
+/// Optimized implementation of multi-scalar multiplication.
+fn msm_bigint(bases: &[Point], bigints: &[Scalar]) -> ProjectivePoint {
+    let size = ark_std::cmp::min(bases.len(), bigints.len());
+    let scalars = &bigints[..size];
+    let bases = &bases[..size];
+    let scalars_and_bases_iter = scalars.iter().zip(bases).filter(|(s, _)| !s.is_zero());
+
+    let c = if size < 32 {
+        3
+    } else {
+        ln_without_floats(size) + 2
+    };
+
+    let num_bits = ScalarField::MODULUS_BIT_SIZE as usize;
+    let fr_one = ScalarField::one().into_bigint();
+
+    let zero = ProjectivePoint::zero();
+    let window_starts: Vec<_> = (0..num_bits).step_by(c).collect();
+
+    // Each window is of size `c`.
+    // We divide up the bits 0..num_bits into windows of size `c`, and
+    // in parallel process each such window.
+    let window_sums: Vec<_> = ark_std::cfg_into_iter!(window_starts)
+        .map(|w_start| {
+            let mut res = zero;
+            // We don't need the "zero" bucket, so we only have 2^c - 1 buckets.
+            let mut buckets = vec![zero; (1 << c) - 1];
+            // This clone is cheap, because the iterator contains just a
+            // pointer and an index into the original vectors.
+            scalars_and_bases_iter.clone().for_each(|(&scalar, base)| {
+                if scalar == fr_one {
+                    // We only process unit scalars once in the first window.
+                    if w_start == 0 {
+                        res += base;
+                    }
+                } else {
+                    let mut scalar = scalar;
+
+                    // We right-shift by w_start, thus getting rid of the
+                    // lower bits.
+                    scalar.divn(w_start as u32);
+
+                    // We mod the remaining bits by 2^{window size}, thus taking `c` bits.
+                    let scalar = scalar.as_ref()[0] % (1 << c);
+
+                    // If the scalar is non-zero, we update the corresponding
+                    // bucket.
+                    // (Recall that `buckets` doesn't have a zero bucket.)
+                    if scalar != 0 {
+                        buckets[(scalar - 1) as usize] += base;
+                    }
+                }
+            });
+
+            // Compute sum_{i in 0..num_buckets} (sum_{j in i..num_buckets} bucket[j])
+            // This is computed below for b buckets, using 2b curve additions.
+            //
+            // We could first normalize `buckets` and then use mixed-addition
+            // here, but that's slower for the kinds of groups we care about
+            // (Short Weierstrass curves and Twisted Edwards curves).
+            // In the case of Short Weierstrass curves,
+            // mixed addition saves ~4 field multiplications per addition.
+            // However normalization (with the inversion batched) takes ~6
+            // field multiplications per element,
+            // hence batch normalization is a slowdown.
+
+            // `running_sum` = sum_{j in i..num_buckets} bucket[j],
+            // where we iterate backward from i = num_buckets to 0.
+            let mut running_sum = ProjectivePoint::zero();
+            buckets.into_iter().rev().for_each(|b| {
+                running_sum += &b;
+                res += &running_sum;
+            });
+            res
+        })
+        .collect();
+
+    // We store the sum for the lowest window.
+    let lowest = *window_sums.first().unwrap();
+
+    // We're traversing windows from high to low.
+    lowest
+        + &window_sums[1..]
+            .iter()
+            .rev()
+            .fold(zero, |mut total, sum_i| {
+                total += sum_i;
+                for _ in 0..c {
+                    total.double_in_place();
+                }
+                total
+            })
+}
 
 pub fn gen_random_vectors<R: RngCore>(n: usize, rng: &mut R) -> Instance {
     let num_bytes = bls377::Fr::zero().serialized_size(Compress::Yes);
@@ -229,11 +332,7 @@ where
         let mut total_duration = Duration::ZERO;
         for i in 0..iterations {
             let start = Instant::now();
-            let result: bls377::G1Projective =
-                ark_ec::scalar_mul::variable_base::VariableBaseMSM::msm_bigint(
-                    &points[..],
-                    &scalars[..],
-                );
+            let result: bls377::G1Projective = msm_bigint(&points[..], &scalars[..]);
             let time = start.elapsed();
             writeln!(&mut output_file, "iteration {}: {:?}", i + 1, time)?;
             result.serialize_uncompressed(&mut output_result_file)?;
